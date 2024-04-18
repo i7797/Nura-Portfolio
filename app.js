f76aab90a7822ef82fbc45076ed784ca505175b1
nura <nooraliabdalhossen@gmail.com>
2024-04-18 14:11:35 +0300

    emailjs

--- app.js
+++ app.js
@@ -8,3 +8,31 @@ function myButton() {
     }
     
 }
+
+function sendEmail(){
+  var params ={
+    name:document.getElementById("name").value,
+   email:document.getElementById("email").value,
+  message:document.getElementById("message").value,
+
+  };
+
+}
+
+ const serviceID ="service_l62o2rm";
+ const templateID ="template_diip4bi";
+ 
+
+ emailjs.send(serviceID,templateID)
+ .then((res)=>{
+
+
+    document.getElementById("name").value ="",
+   document.getElementById("email").value ="",
+   document.getElementById("message").value ="",
+   console.log(res);
+   alert("done")
+ 
+
+ })
+ .catch((err)=>console.log(err));
